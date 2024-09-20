8bd675ad476374d076621db54aba729fab9c0d21
lomousmane <lomousmane@gmail.com>
2024-09-20 19:52:15 +0000

    analyse_desc

---
+++ semaine_1/evaluation_paire.R
@@ -0,0 +1,35 @@
+library(readr)
+install.packages("DescTools")
+library(DescTools)
+# importation des données
+satisfaction_hopital <- read_csv2("C:/mes dossiers/Formation_fun_mooc_2024/données/satisfaction_hopital.csv")
+View(satisfaction_hopital)
+# structure
+str(satisfaction_hopital)
+dim(satisfaction_hopital)
+# les trois variables catégorielles (sexe,service,prof)
+# Présentons les pourcentage sexe
+tab=table(satisfaction_hopital$sexe)
+tab
+round(prop.table(tab),3)
+# Présentons les pourcentage service
+tab=table(satisfaction_hopital$service)
+tab
+round(prop.table(tab),3)
+# Présentons les pourcentage prof
+tab=table(satisfaction_hopital$profession)
+tab
+round(prop.table(tab),3)
+# Resumes des variables numériques
+install.packages("prettyR")
+library(prettyR)
+describe(satisfaction_hopital[c("age","amelioration.sante",
+                                  "amelioration.moral","recommander","score.relation",
+                                "score,information")],num.desc = c("min","max","mean" ,"median","sd","valid.n"))
+
+# Faites un histogramme du score de relation (score.relation).
+hist(satisfaction_hopital$score.relation, xlab="score relation
+    ",ylab="frequence",main="Histogramme score relation",col="blue")
+# boxplot la distribution du score de relation chez les hommes et les femmes.
+boxplot(satisfaction_hopital$score.relation~satisfaction_hopital$sexe,xlab="sexe",ylab="score relation",
+        ,main="Diagramme en boxplot",col=rainbow(10))
---
+++ semaine_1/semaine_1.R
@@ -0,0 +1,32 @@
+# importer les données
+library(readr)
+library(tidyverse)
+library(prettyR)
+smp_1=read.csv2("C:/mes dossiers/Formation_fun_mooc_2024/données/smp1.csv",header=T)
+smp_1
+# structure des données
+str(smp_1)
+# dimension
+dim(smp_1)
+# le nom des colonnes
+names(smp_1)
+# Analyse descriptives des données
+describe(smp_1)
+# Calculer le nombre de detenu ayant u metier
+tab_prof=table(smp_1$prof)
+# un diagramme en bar
+pie(tab_prof,col=rainbow(10),main="Distribution de la variable profession")
+# Histogramme de l'age
+ggplot(smp_1,aes(x=age))+
+                  geom_histogram(fill="blue",binwidth = 5)+
+                  ggtitle("Diagramme en Histogramme de l'age")
+# Diagramme en boxplot
+ggplot(smp_1,aes(y=age))+
+                  geom_boxplot()+
+                  ggtitle("Diagramme en boxplot de l'age")
+# Nuage de point
+plot(smp_1$age,smp_1$n.enfant,xlab="age",ylab="enfant",
+     main="nuage de point")
+savehistory("commande.R")
+hist(smp_1$age)
+lines(density(smp_1$age,na.rm = T),col="red")
