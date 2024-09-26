fc32b44a4c13ff24b46ac1e92f586898bc74a6fe
lomousmane <lomousmane@gmail.com>
2024-09-26 11:24:24 +0000

    pca

---
+++ semaine_5/analyse_multidimensionelle.R
@@ -0,0 +1,26 @@
+setwd("C:/mes dossiers/Formation_fun_mooc_2024/données")
+smp=read.csv2("smp2.csv")
+# structure des données
+str(smp)
+list(colnames(smp))
+var<-c("age","n.enfant","dep.cons","scz.cons",
+       "grav.cons","rs","ed","dr")
+# matrice de correlation
+cor<-cor(smp[,var],use="complete.obs")
+# visualiser avec corrplot
+install.packages("corrplot")
+library(corrplot)
+corrplot(cor,method="circle")
+# ACP
+install.packages("psy")
+library(psy)
+# Projection de  circulaire
+psy::mdspca(smp[,var])
+# projection sphérique
+psy::sphpca(smp[,var],v=55)
+# Relation entre variable expliquer et explicatives
+expliquer<-"grav.cons"
+explicatives<-c("age","n.enfant","dep.cons","scz.cons"
+                     ,"rs","ed","dr")
+? fpca
+fpca(y=expliquer,x=explicatives,data=smp,partial = "No")
