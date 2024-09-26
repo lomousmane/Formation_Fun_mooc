f395b2926bd155a8ac390055dd9913b93dc78c84
lomousmane <lomousmane@gmail.com>
2024-09-26 12:22:32 +0000

    cah with factominer

--- semaine_5/Classification_hierarchique.R
+++ semaine_5/Classification_hierarchique.R
@@ -10,3 +10,24 @@ var<-c("age","n.enfant","dep.cons","scz.cons",
 cah=hclust(dist(t(scale(smp[,var]))),method="ward.D2")
 cah
 plot(cah,main="cluster dendogramme")
+# install factominer et factoextra
+library(factoextra)
+library(FactoMineR)
+library(missMDA)
+# Compute PCA with ncp = 3
+nb <- estim_ncpPCA(smp[,var],ncp.min=0,ncp.max=5,method.cv="Kfold",nbsim=50)
+imputed <- imputePCA(smp[,var],ncp=nb$ncp)
+res.pca <- PCA(imputed$completeObs)
+# hierarchical class principal component
+res_cah<-HCPC(res.pca,graph = F)
+plot(res_cah)
+library(ggpubr)
+fviz_cluster(res_cah,
+             repel = TRUE, # Avoid label overlapping
+             show.clust.cent = TRUE, # Show cluster centers
+             palette = "jco", # Color palette see ?
+             #ggpubr::ggpar
+             ggtheme = theme_minimal(),
+             main = "Factor map"
+)
+
